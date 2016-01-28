657afae747095f4ac38d018d8059c287e7e02a36
hreid22 <hreid@DN0a223511.SUNet>
2016-01-28 13:39:12 -0800

    update shakespeare

--- exercises/0004-shakefiles/b.py
+++ exercises/0004-shakefiles/b.py
@@ -1,8 +1,11 @@
 import os
 import requests
+url= "http://stash.compciv.org/scrapespeare/matty.shakespeare.tar.gz"
 resp = requests.get("http://stash.compciv.org/scrapespeare/matty.shakespeare.tar.gz")
 shakename = os.path.join('tempdata', 'matty.shakespeare.tar.gz')
 zfile = open(shakename, 'wb')
 respdata= resp.content
 zfile.write(respdata)
 zfile.close
+print("Downloading:", url)
+print("Writing file:", shakename)
